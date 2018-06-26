57216ff7f1be9195c6c9b4b9d8e03d64032fd11e
cyril jardin <cyilroc60@hotmail.fr>
2018-06-26 16:00:28 +0200

    p3js2

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//déclaration de variable
+var text = document.getElementById('text');
+//déclaration de la fonction affichage
+function affi(){
+  text.style.display = 'block';
+}
+//déclaration de la fontion cacher
+function masq(){
+  text.style.display = 'none';
+}
